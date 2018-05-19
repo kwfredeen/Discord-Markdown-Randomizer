ef306260cd3fa6ebd742d29f270505513294feff
kwfredeen <kwfredeen@gmail.com>
2018-05-18 20:15:54 -0400

    First commit

--- DiscordMarkdownRandomizer/Form1.Designer.cs
+++ DiscordMarkdownRandomizer/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(32, 64);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(552, 256);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(608, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(552, 256);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(616, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 72);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Randomize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1185, 430);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Discord Markdown Randomizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DiscordMarkdownRandomizer/Form1.cs
+++ DiscordMarkdownRandomizer/Form1.cs
@@ -16,5 +16,122 @@ namespace DiscordMarkdownRandomizer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            string userInput = inputTextBox.Text;
+            string outputString = "";
+
+            int randOut;
+            int lastRand = 8; //rand will never output 8
+            foreach(char c in userInput)
+            {
+                randOut = rand.Next(0,7);
+
+                if(c == ' ')
+                {
+                    //Dont do anything with spaces, just concat and continue
+                    outputString = outputString + c;
+                    continue;
+                }
+
+                switch (randOut)
+                {
+                    case 0:
+                        //normal text
+                        outputString = outputString + c;
+                        lastRand = randOut;
+                        continue;
+                    case 1:
+                        //italics
+                        if (lastRand == 1)
+                        {
+                            //already italics, inserts c into last italics block
+                            outputString = outputString.Insert(outputString.LastIndexOf('*'), c.ToString());
+                            lastRand = randOut;
+                            continue;
+                        }
+                        outputString = outputString + "*" + c + "* ";
+                        lastRand = randOut;
+                        continue;
+                    case 2:
+                        //bold
+                        if (lastRand == 2)
+                        {
+                            //already bold, inserts c into last bold block
+                            outputString = outputString.Insert(outputString.LastIndexOf('*') - 1, c.ToString());
+                            lastRand = randOut;
+                            continue;
+                        }
+                        outputString = outputString + "**" + c + "** ";
+                        lastRand = randOut;
+                        continue;
+                    case 3:
+                        //underline
+                        if (lastRand == 3)
+                        {
+                            //already underline, inserts c into last underline block
+                            outputString = outputString.Insert(outputString.LastIndexOf('_') - 1, c.ToString());
+                            lastRand = randOut;
+                            continue;
+                        }
+                        outputString = outputString + "__" + c + "__ ";
+                        lastRand = randOut;
+                        continue;
+                    case 4:
+                        //bold italics
+                        if (lastRand == 4)
+                        {
+                            //already bold italics, inserts c into last bold italics block
+                            outputString = outputString.Insert(outputString.LastIndexOf('*') - 2, c.ToString());
+                            lastRand = randOut;
+                            continue;
+                        }
+                        outputString = outputString + "***" + c + "*** ";
+                        lastRand = randOut;
+                        continue;
+                    case 5:
+                        //underline bold
+                        if (lastRand == 5)
+                        {
+                            //already underline bold, inserts c into last underline bold block
+                            outputString = outputString.Insert(outputString.LastIndexOf('*') - 1, c.ToString());
+                            lastRand = randOut;
+                            continue;
+                        }
+                        outputString = outputString + "__**" + c + "**__ ";
+                        lastRand = randOut;
+                        continue;
+                    case 6:
+                        //underline italics
+                        if (lastRand == 6)
+                        {
+                            //already underline italics, inserts c into last underline italics block
+                            outputString = outputString.Insert(outputString.LastIndexOf('*'), c.ToString());
+                            lastRand = randOut;
+                            continue;
+                        }
+                        outputString = outputString + "__*" + c + "*__ ";
+                        lastRand = randOut;
+                        continue;
+                    case 7:
+                        //Underline bold italics
+                        if (lastRand == 1)
+                        {
+                            //already underline bold italics, inserts c into last underline bold italics block
+                            outputString = outputString.Insert(outputString.LastIndexOf('*') - 2, c.ToString());
+                            lastRand = randOut;
+                            continue;
+                        }
+                        outputString = outputString + "__***" + c + "***__ ";
+                        lastRand = randOut;
+                        continue;
+
+                }
+            }
+
+            textBox1.Text = outputString;
+        }
     }
 }
